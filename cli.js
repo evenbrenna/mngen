4ee37c93d01c5269fce77d0f34752522b71ea7d2
Leonardo Kewitz <leokewitz@gmail.com>
2017-11-11 02:28:16 -0200

    refact: Refactor main module structure using ES6

--- cli.js
+++ cli.js
@@ -1,38 +1,37 @@
 #!/usr/bin/env node
-'use strict';
-var meow = require('meow');
-var mnGen = require('./');
+'use strict'
 
-var cli = meow({
-	help: [
-		'Examples',
-		'  $ mngen',
-		'  tokyo',
-		'',
-		'  $ mngen --glue 2',
-		'  english-legal',
-		'',
-		'  $ mngen --glue 2 --separator /',
-		'  dexter/toronto',
-		'',
-    '  $ mngen --list 3',
-    '  canoe amigo kermit',
-    '',
-    '  $ mngen --list 2 --glue 3',
-    '  oxford-soviet-rubber shave-deal-freddie',
-    '',
-		'Options',
-		'  --list [number]  Get a whitespace separated list of words',
-		'  --glue [number]  Make each word a dash-joined combo of words',
-		'  --separator [string]  Character used to join combo of words'
-	]
-});
+const meow = require('meow')
+const mnGen = require('./')
 
-var list = typeof cli.flags.list === 'boolean' ? 100 : cli.flags.list;
-var glue = cli.flags.glue || 1;
-var separator = cli.flags.separator || '-'
+const help = `
+Examples
+	$ mngen
+	tokyo
 
-console.log(cli.flags.list
-	? mnGen.list(list, glue, separator).join(' ')
-	: mnGen.word(glue, separator)
-);
+	$ mngen --glue 2
+	english-legal
+
+	$ mngen --glue 2 --separator /
+	dexter/toronto
+
+	$ mngen --list 3
+	canoe amigo kermit
+
+	$ mngen --list 2 --glue 3
+	oxford-soviet-rubber shave-deal-freddie
+
+Options,
+	--list [number]  Get a whitespace separated list of words
+	--glue [number]  Make each word a dash-joined combo of words
+	--separator [string]  Character used to join combo of words`
+
+const cli = meow({ help })
+
+const {
+	list = 1,
+	glue = 1,
+	separator = '-',
+} = cli.flags
+
+console.log(mnGen(list, glue, separator).join(' '))
--- index.js
+++ index.js
@@ -1,68 +1,43 @@
-'use strict';
-var words = require('./words.json');
+'use strict'
 
-// Get a random word
-var getRandomWord = function() {
-  return words[Math.floor(Math.random() * words.length)];
-};
+const assert = require('assert')
+const words = require('./words.json')
 
-// Join two words with a dash. False if word already contains newWord.
-var dashJoin = function(word, newWord, separator) {
-  if (word.indexOf(newWord) > -1) { return false; }
-  return word + separator + newWord;
-};
+const getUniqueWords = (n) => {
+  const uniqueWords = []
+  do {
+    const word = words[Math.floor(Math.random() * words.length)]
+    if (uniqueWords.indexOf(word) === -1) uniqueWords.push(word)
+  } while (uniqueWords.length < n)
+  return uniqueWords
+}
 
 // Calculate number of possible unique list items given combos of n words
 // Reaches Number.MAX_VALUE at n === 97 and returns Infinity for n >= 97
-var maxLength = function(n) {
-  var max = words.length;
+const maxLength = (n) => {
+  let max = words.length
   while (n > 1) {
-    max *= words.length - n + 1;
-    n--;
+    max *= words.length - n + 1
+    n--
   }
-  return max;
-};
+  return max
+}
 
-// Get a random word / word-combo
-var word = function(glue, separator) {
-  glue = glue || 1;
-  separator = separator || '-';
-  if (typeof glue !== 'number') { throw new Error('Not A Number!'); }
-  var word = getRandomWord();
+// Get an array of unique words / Comword-combos
+const gen = (length = 100, glue = 1, separator = '-') => {
+  assert(
+    length <= maxLength(glue),
+    new Error(`Max list length for [glue] === ${glue} is ${maxLength(glue)}`)
+  )
+  const wordCombos = []
+  do {
+    const word = getUniqueWords(glue).join(separator)
+    if (wordCombos.indexOf(word) === -1) wordCombos.push(word)
+  } while (wordCombos.length < length)
+  return wordCombos
+}
 
-  if (glue > 1) {
-    for (var i = 1; i < glue; i++) {
-      var uniqueCombo = false;
-      while (!uniqueCombo) { uniqueCombo = dashJoin(word, getRandomWord(), separator); }
-      word = uniqueCombo;
-    }
-  }
-  return word;
-};
+gen.list = gen
+gen.word = (glue, separator) => gen(1, glue, separator)[0]
 
-// Get an array of unique words / word-combos
-var list = function(length, glue, separator) {
-  if (length > maxLength(glue)) {
-    throw new Error('Max list length for [glue] === ' + glue +
-                    ' is ' + maxLength(glue));
-  }
-
-  length = length || 100;
-  glue = glue || 1;
-  separator = separator || '-';
-
-  var returnWords = [];
-
-  for (var i = 0; i < length; i++) {
-    var newWord = word(glue, separator);
-    if (returnWords.indexOf(newWord) === -1) {
-      returnWords.push(newWord);
-    } else {
-      i--;
-    }
-  }
-  return returnWords;
-};
-
-exports.word = word;
-exports.list = list;
+module.exports = gen
