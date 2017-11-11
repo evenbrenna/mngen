e32c0137f911054643667a47f6939ce73f48c84c
Leonardo Kewitz <leokewitz@gmail.com>
2017-11-11 02:28:29 -0200

    test: Refact tests using ES6

--- test.js
+++ test.js
@@ -1,50 +1,41 @@
-'use strict';
+'use strict'
 
-var test = require('ava');
-var mnGen = require('./');
-var words = require('./words.json');
+const test = require('ava')
+const mnGen = require('./')
+const words = require('./words.json')
 
-test(function (t) {
-	t.assert(mnGen.word().length > 0);
-	t.assert(mnGen.list().length === 100);
-	t.assert(mnGen.list(10).length === 10);
-	t.assert(mnGen.list(1644, 2).length === 1644);
-	t.assert(mnGen.list(1, 2)[0].indexOf('-') !== -1);
-	t.assert(mnGen.word(2).indexOf('-') !== -1);
-	t.assert(mnGen.word(4).split('-').length === 4);
-	t.end();
-});
+test('basic generators', t => {
+	t.assert(mnGen().length === 100)
+	t.assert(mnGen(10).length === 10)
+	t.assert(mnGen(1644, 2).length === 1644)
+	t.assert(mnGen(1, 2)[0].indexOf('-') !== -1)
+  t.assert(mnGen.word().length > 0);
+  t.assert(mnGen.list().length === 100);
+  t.assert(mnGen.list(10).length === 10);
+  t.assert(mnGen.list(1644, 2).length === 1644);
+  t.assert(mnGen.list(1, 2)[0].indexOf('-') !== -1);
+  t.assert(mnGen.word(2).indexOf('-') !== -1);
+  t.assert(mnGen.word(4).split('-').length === 4);
+})
 
-test(function(t) {
-	t.plan(1);
-
-	try {
-		mnGen.list(1644);
-	} catch (err) {
-		t.assert(new RegExp(words.length).test(err.message));
-	}
-
-	t.end();
-});
-
-test(function(t) {
-	t.plan(1);
+test('max combinations', t => {
+	t.plan(1)
 
 	try {
-		mnGen.list(2665057, 2);
+		mnGen(2665057, 2)
 	} catch (err) {
-		t.assert(new RegExp(words.length * (words.length - 1)).test(err.message));
+		t.assert(new RegExp(words.length * (words.length - 1)).test(err.message))
 	}
 
-	t.end();
-});
+	try {
+		mnGen(1644)
+	} catch (err) {
+		t.assert(new RegExp(words.length).test(err.message))
+	}
+})
 
-test('custom separator', function (t) {
-	const word = mnGen.word(2, '/');
-	const list = mnGen.list(1, 2, '/')[0];
-	t.assert(word.indexOf('-') === -1);
-	t.assert(word.indexOf('/') !== -1);
-	t.assert(list.indexOf('-') === -1);
-	t.assert(list.indexOf('/') !== -1);
-	t.end();
-});
+test('custom separator', t => {
+	const list = mnGen(1, 2, '/')[0]
+	t.assert(list.indexOf('-') === -1)
+	t.assert(list.indexOf('/') !== -1)
+})
